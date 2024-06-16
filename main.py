fe553f08d930449c987031c64c9a722f9054558e
Toortles <64560777+Toortles@users.noreply.github.com>
2024-06-16 02:39:18 -0700

    Running window with the correct color and everything

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+import pygame
+
+# PyGame setup
+pygame.init()
+pygame.display.set_caption("Birds N' Planes")
+screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, 32, vsync=True)
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((135, 206, 235))
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+pygame.quit()
