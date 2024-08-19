b9489f7d09fa1a9dd19f9fca6077e195da8961c4
Toortles <scmischler@outlook.com>
2024-08-19 02:12:59 -0700

    Inital commit, Includes Player class, a few files, setup and housekeeping

--- main.py
+++ main.py
@@ -1,40 +1,41 @@
 import pygame
 
-# PyGame Initialization
-# pygame.init()
-player_pos = pygame.Vector2(150, 350)
-
-# Imports
-program_icon = pygame.image.load("images/icon.png")
-
-player_rotation = pygame.Surface((32, 32))
-player_rotation.fill((255, 255, 255, 0))
-player_icon = pygame.image.load("images/player.png")
-
-# Game setup
-pygame.display.set_icon(program_icon)
+# Window Setup
 pygame.display.set_caption("Birds N' Planes")
-screen = pygame.display.set_mode((1280, 720), pygame.RESIZABLE, 32, vsync=True)
+pygame.display.set_icon(pygame.image.load("images/icon.png"))
+
+# PyGame setup
+pygame.init()
+screen = pygame.display.set_mode((800, 600), pygame.RESIZABLE, 32, vsync=True)
 clock = pygame.time.Clock()
 running = True
+
+
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("images/player.png").convert_alpha()
+        self.rect = self.image.get_rect()
+        self.rect.x += 35
+
+    def update(self):
+        if pygame.key.get_pressed()[pygame.K_UP]:
+            self.rect.move_ip(0, -2)
+        elif pygame.key.get_pressed()[pygame.K_DOWN]:
+            self.rect.move_ip(0, 2)
+
+
+player = Player()
 
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    screen.fill((135, 206, 235))
+    screen.fill([135, 206, 235])
 
-    screen.blit(player_rotation, player_pos)
-
-    if pygame.key.get_pressed()[pygame.K_w] or pygame.key.get_pressed()[pygame.K_UP]:
-        player_rotation = pygame.transform.rotate(player_icon, 5)
-        player_pos = player_pos - pygame.Vector2(0, 5)
-    elif pygame.key.get_pressed()[pygame.K_s] or pygame.key.get_pressed()[pygame.K_DOWN]:
-        player_pos = player_pos + pygame.Vector2(0, 5)
-        player_rotation = pygame.transform.rotate(player_icon, -5)
-    else:
-        player_rotation = player_icon
+    screen.blit(player.image, player.rect)
+    player.update()
 
     pygame.display.flip()
 
